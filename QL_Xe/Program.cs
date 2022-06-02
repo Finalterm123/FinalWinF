809a47fd607dc53ad31be371b920906aee2fb4bd
Cong Tu <phancongtu25032002@gmail.com>
2022-06-02 12:08:55 +0700

    Tu test

---
+++ QL_Xe/Program.cs
@@ -0,0 +1,29 @@
+﻿using QL_Xe.TrongCoiXe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_Xe
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        ///Nồng ngu lắm!
+        /////TÚ ĐÂU RỒI????
+        /// Tú đây
+        /// Lại là TÚ đây
+        //TRUNG NÈ BẠN!
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login_Form());
+
+        }
+    }
+}
